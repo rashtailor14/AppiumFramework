07cf9cb8720de732e54d8dfb03c02cfb7f1e2d0e
rashm <rashmitailor24@gmail.com>
2021-02-03 01:30:25 -0500

    first commit

---
+++ src/test/java/qaacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("NativeAppAndroid");
+	}
+	
+	@Test
+	public void iOSApps()
+	{
+		System.out.println("iOSApps");
+	}
+}
